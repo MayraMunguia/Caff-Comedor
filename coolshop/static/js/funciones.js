e3a0d6aceda494eead52a11019d0bd1bb40b9691
Mayra Esperanza Munguía Palma <mayrahopem@gmail.com>
2017-12-07 17:18:05 -0700

    cambios en javascriot

--- coolshop/static/js/funciones.js
+++ coolshop/static/js/funciones.js
@@ -1,16 +1,11 @@
 $(document).ready(function() {
 
+$("button").click(function() {
+    $("#Camb").toggle();
+});
 
-$("Cambiar").click(function(){
-    $("Camb").html("{% for product in products %}
-	<div class="item">
-		<a href="{{ product.get_absolute_url }}">
-		<img src="{% if product.image %}{{ product.image.url }}{% else %}{% static 'img/no_image.png' %}{% endif %}">
-		</a>
-		<a href="{{ product.get_absolute_url }}">{{ product.name }}</a><br>
-		${{ product.price }}
-	</div>
-{% endfor %}");
+$("button").click(function() {
+    $("#productos").css('visibility','visible');
 });
 
 });
