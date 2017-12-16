806747f759e1346a13cbce244cf2c633f058acf4
Mayra Esperanza Munguía Palma <mayrahopem@gmail.com>
2017-12-15 19:02:41 -0700

    agregado boton de cantidad

--- coolshop/static/js/funciones.js
+++ coolshop/static/js/funciones.js
@@ -8,5 +8,23 @@ $("button").click(function() {
     $("#productos").css('visibility','visible');
 })
 
+ $('#numericcontrol input[type="text"]').val("1");
+	$val=$('#numericcontrol input[type="text"]').val();
+	$('#numericcontrol a.plus').click(function(){
+		$val++;
+		$(this).parent('div#numericcontrol').find('input[type="text"]').val($val);
+	});
+	$('#numericcontrol a.minus').click(function(){
+		
+		if($val>1)
+		{
+		$val--;	
+		$(this).parent('div#numericcontrol').find('input[type="text"]').val($val);
+		}
+		else
+		{
+			alert("La cantidad no puede ser menor a 0");
+		}
+	});
 
 });
